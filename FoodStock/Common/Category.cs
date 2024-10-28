a3f8ca8e20e2dc40a96f9aa77e4f825e0267669e
Contii <joaovitorconti@live.com>
2024-10-28 19:28:25 -0300

    Added Category Class

--- FoodStock/Common/Category.cs
+++ FoodStock/Common/Category.cs
@@ -1,6 +1,27 @@
 namespace FoodStock.Model.Common;
 
-public class CategoryModel
+public class CategoryModel(int categoryID, string name, string description)
 {
-
+    public int? CategoryID { get; set; } = categoryID;
+    public string? Name { get; set; } = name;
+    public string? Description { get; set; } = description;
+    
+    public override string ToString()
+    {
+        return $"[{CategoryID}, {Name}, {Description}]";
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is CategoryModel other)
+        {
+            return other.CategoryID == CategoryID;
+        }
+        return false;
+    }
+    
+    public override int GetHashCode()
+    {
+        return CategoryID.GetHashCode();
+    }
 }
