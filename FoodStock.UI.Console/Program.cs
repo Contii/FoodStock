ff7958471441552399812c120ed537780b5a5a47
Contii <joaovitorconti@live.com>
2024-10-28 19:29:06 -0300

    Added UI Console

---
+++ FoodStock.UI.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FoodStock.Model.Common;
+
+Console.WriteLine("Hello, World!");
+ 
+CategoryModel category = new (1, "Grain", "Arroz");
+CategoryModel category2 = new (1, "Grain", "Arroz");
+
+
+Console.Write(category == category2);
