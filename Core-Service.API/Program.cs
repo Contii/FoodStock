14d982885431662a6cce008e0b274e7b39ba91bd
Conti <joaovitorconti@live.com>
2024-11-03 02:12:41 -0300

    Added Get and Post routes.

--- Core-Service.API/Program.cs
+++ Core-Service.API/Program.cs
@@ -24,4 +24,26 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization(); // Auth Middleware, not used in this project but good for future security implementations.
 
+// Define the API CRUD routes.
+// Obtaining all categories.
+app.MapGet("/api/categories", async (EFCoreContext context) =>
+{
+    return await context.Categories.ToListAsync();
+});
+
+// Obtaining a specific category by ID.
+app.MapGet("/api/categories/{id}", async (EFCoreContext context, int id) =>
+{
+    var category = await context.Categories.FindAsync(id); // Verify if the category exists.
+    return category is not null ? Results.Ok(category) : Results.NotFound();
+});
+
+// Create a new category.
+app.MapPost("/api/categories", async (EFCoreContext context, CategoryModel category) =>
+{
+    context.Categories.Add(category); // Add the new category to the context.
+    await context.SaveChangesAsync(); // Save the changes to the database.
+    return Results.Created($"/api/categories/{category.CategoryID}", category);
+});
+
 app.Run();
