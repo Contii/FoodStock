9185a5763e46b7d951a5f9f0fff42d02f583a673
Conti <joaovitorconti@live.com>
2024-11-04 03:18:07 -0300

    Added ItemModel entity and comments.

--- Persistence/EFCoreContext.cs
+++ Persistence/EFCoreContext.cs
@@ -6,9 +6,10 @@ namespace FoodStock.Persistence;
 public class EFCoreContext : DbContext
 {
     public DbSet<CategoryModel>  Categories { get; set; }
+    public DbSet<ItemModel> Items { get; set; }
 
     // public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options)
-    public EFCoreContext( ) // Uncomment this and comment above line to create migration files.
+    public EFCoreContext( ) // Uncomment this and comment above line to create/update migration files.
     {    
     }
 
@@ -18,10 +19,27 @@ public class EFCoreContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
-        .Entity<CategoryModel>(
+        .Entity<CategoryModel>( // Define the CategoryModel entity.
             eb =>
             {
-                eb.HasKey(pk => pk.CategoryID);
+
+                eb.HasKey(pk => pk.CategoryID); // CategoryID is the primary key in the database.
+                eb.Property(p => p.Name).IsRequired(); // Name is required in the database.
+                eb.Property(p => p.Description).HasMaxLength(100);
+                eb.HasMany(p => p.Items) // A category can have many items in the database.
+                    .WithOne(p => p.Category) // An item belongs to a category in the database.
+                    .HasForeignKey(p => p.CategoryID); // The foreign key of the item is the CategoryID.
+            });
+        modelBuilder.Entity<ItemModel>( // Define the ItemModel entity.
+            eb =>
+            {
+                eb.HasKey(pk => pk.ItemID);
+                eb.Property(p => p.Name).IsRequired();
+                eb.Property(p => p.Measure).IsRequired();
+                eb.Property(p => p.MeasureType).IsRequired();
+                eb.HasOne(p => p.Category)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(p => p.CategoryID);
             });
 
         base.OnModelCreating(modelBuilder);
