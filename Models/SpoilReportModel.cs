0a72c32a2c89066879ab0b0c813abfa1dbb0e6ca
Conti <joaovitorconti@live.com>
2025-01-26 00:33:15 -0300

    Implemented SpoilReportModel.

---
+++ Models/SpoilReportModel.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FoodStock.Models;
+
+public class SpoilReportModel
+{
+    public int SpoilReportID { get; set; }
+
+    [Required]
+    [EnumDataType(typeof(ReportTypeEnum))]
+    public ReportTypeEnum ReportType { get; set; }
+
+    [JsonIgnore]
+    public List<ItemModel> Itens { get; set; } = new List<ItemModel>(); // Initialize with a default value
+
+    public SpoilReportModel(int spoilReportID, ReportTypeEnum reportType, List<ItemModel> itens)
+    {
+        SpoilReportID = spoilReportID;
+        ReportType = reportType;
+        Itens = itens;
+    }
+
+    public SpoilReportModel()
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"[{SpoilReportID}, {ReportType}]";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is SpoilReportModel other)
+        {
+            return other.SpoilReportID == SpoilReportID;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return SpoilReportID;
+    }
+
+}
